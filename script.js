4394f9cf9b05964e592e16291dbe904b26584e69
Robin WSL <robin.daub1@gmail.com>
2024-10-27 12:30:43 +0100

    Add grid and eventlistener for mouseover & mouseout

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const grid_size = 16;
 let grid = [];
 let container = document.querySelector(".container");
 
-for (let i = 0; i < grid_size; i++) {
-	grid[i] = document.createElement("div");
-	grid[i].classList.add("grid-cell");
-	container.appendChild(grid[i]);
+function mouseHover() {
+
 }
+
+// Create Grid using Flexbox
+for (let i = 0; i < grid_size*grid_size; i++) {
+	grid[i] = document.createElement("div");
+	//grid[i].classList.add("grid-cell");
+	grid[i].classList.add("square");
+	grid[i].addEventListener("mouseover", () => {
+		grid[i].classList.add("active");
+	 });
+	grid[i].addEventListener("mouseout", () => {
+		grid[i].classList.remove("active");
+	 });
+	container.appendChild(grid[i]);
+
+}
+
