186544ff567a93632c922ad196fbd1e09d2e862f
Robin WSL <robin.daub1@gmail.com>
2024-10-27 13:36:15 +0100

    add responsive grid, reset button

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-const grid_size = 16;
+let grid_size = 16;
 let grid = [];
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const button = document.querySelector("#reset");
 
-function mouseHover() {
+button.addEventListener("click", () => {
+	let size = +prompt("Set Grid Size to: ", "16");
+	if (0 < size && size < 101 && !isNaN(size)) {
+		console.log("input is valid");
+		console.log(size);
+		createGrid(size);
+	} else {
+		alert("Input is not Valid! Must be a Number 1 - 100")
+	}
 
+});
+
+function createGrid(size) {
+	// Create Grid using Flexbox
+	grid = [];
+	const percentage = calcBasisPercentage(size);
+	for (let i = 0; i < size * size; i++) {
+		grid[i] = document.createElement("div");
+		//grid[i].classList.add("grid-cell");
+		grid[i].classList.add("square");
+		grid[i].style.flex = `1 0 ${percentage}%`;
+		grid[i].addEventListener("mouseover", () => {
+			grid[i].classList.add("active");
+		});
+		grid[i].addEventListener("mouseout", () => {
+			grid[i].classList.remove("active");
+		});
+		container.appendChild(grid[i]);
+	}
 }
 
-// Create Grid using Flexbox
-for (let i = 0; i < grid_size*grid_size; i++) {
-	grid[i] = document.createElement("div");
-	//grid[i].classList.add("grid-cell");
-	grid[i].classList.add("square");
-	grid[i].addEventListener("mouseover", () => {
-		grid[i].classList.add("active");
-	 });
-	grid[i].addEventListener("mouseout", () => {
-		grid[i].classList.remove("active");
-	 });
-	container.appendChild(grid[i]);
-
+function calcBasisPercentage(n) {
+	return Math.round((100 / n) * 100) / 100;
+	//return Math.round(100 / n);
 }
 
+//createGrid(grid_size);
